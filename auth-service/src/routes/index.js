07847ab55cbd1f218d1fcae43ee36ebecb4b0368
nuttapong boonsala <nuttapongb@thinknet.co.th>
2024-02-07 13:13:28 +0700

    add new definition

--- auth-service/src/routes/index.js
+++ auth-service/src/routes/index.js
@@ -113,6 +113,112 @@ const router = express.Router()
  *         type: string
  *         enum: ['STUDENT', 'OFFICE_WORKER', 'TOURIST', 'DIGITAL_NORMAD', 'TAKEAWAY']
  * 
+ *   ShopInput:
+ *     type: object
+ *     required:
+ *       - username
+ *       - password
+ *       - name
+ *       - token
+ *     properties:
+ *       username:
+ *         type: string
+ *       password:
+ *         type: string
+ *       name:
+ *         type: string
+ *       description:
+ *         type: string
+ *       address:
+ *         type: object
+ *         properties:
+ *           country:
+ *             type: string
+ *           province:
+ *             type: string
+ *           district:
+ *             type: string
+ *           subDistrict:
+ *             type: string
+ *           road:
+ *             type: string
+ *           postelCode:
+ *             type: string
+ *           addressText:
+ *             type: string
+ *       menus:
+ *         type: array
+ *         items:
+ *           properties:
+ *             name:
+ *               type: string
+ *             category:
+ *               type: string
+ *             price:
+ *               type: number
+ *             description:
+ *               type: string
+ *             image:
+ *               type: string
+ *       coverImage:
+ *         type: string
+ *       shopImage:
+ *         type: array
+ *         collectionFormat: csv
+ *         items:
+ *           type: string
+ *       menuImages:
+ *         type: array
+ *         collectionFormat: csv
+ *         items:
+ *           type: string
+ *       foodImages:
+ *         type: array
+ *         collectionFormat: csv
+ *         items:
+ *           type: string
+ *       otherImages:
+ *         type: array
+ *         collectionFormat: csv
+ *         items:
+ *           type: string
+ *       daysOpen:
+ *         type: array
+ *         collectionFormat: csv
+ *         items:
+ *           type: number
+ *       timeOpen:
+ *         type: string
+ *       timeClose:
+ *         type: string
+ *       singleSeat:
+ *         type: number
+ *       doubleSeat:
+ *         type: number
+ *       largeSeat:
+ *         type: number
+ *       wifi:
+ *         type: boolean
+ *       powerPlugs:
+ *         type: boolean
+ *       conferenceRoom:
+ *         type: boolean
+ *       toilet:
+ *         type: boolean
+ *       smokingZone:
+ *         type: boolean
+ *       photoSpots:
+ *         type: string
+ *         enum: ['FEW', 'MEDIUM', 'MUCH']
+ *       noice:
+ *         type: string
+ *         enum: ['QUITE', 'NORMAL']
+ *       customerGroup:
+ *         type: string
+ *         enum: ['STUDENT', 'OFFICE_WORKER', 'TOURIST', 'DIGITAL_NORMAD', 'TAKEAWAY']
+ *       token:
+ *         type: string 
+ * 
  *   CustomerLogin:
  *     type: object
  *     required:
@@ -159,8 +265,54 @@ const router = express.Router()
  *                type: number
  *              value:
  *                type: string
+ * 
+ *   CustomerInput:
+ *     type: object
+ *     required:
+ *       - username
+ *       - password
+ *       - name
+ *       - token
+ *     properties:
+ *       username:
+ *         type: string
+ *       password:
+ *         type: string
+ *       name:
+ *         type: string
+ *       gender:
+ *         type: string
+ *         enum: [ 'MALE', 'FEMALE' ]
+ *       age:
+ *         type: string
+ *         enum: [ 'UNDER_22', '23_TO_40', '41_TO_60', 'AFTER_61' ]
+ *       occupation:
+ *         type: string
+ *       favourites:
+ *         type: array
+ *         items:
+ *           properties:
+ *             _shopID: 
+ *               type: number
+ *       tags:
+ *         type: array
+ *         items:
+ *            properties:
+ *              key:
+ *                type: number
+ *              value:
+ *                type: string
+ *       token:
+ *         type: string 
+ * 
+ *   Token:
+ *     type: object
+ *     required:
+ *       - token
+ *     properties:
+ *       token:
+ *         type: string 
  */
-
 
 // [ ] task: healthcheck
 
@@ -182,24 +334,30 @@ router.get('/livez', async(req, res) => res.status(200).json({ status: 'ok' }))
 /**
  * @swagger
  * paths:
- *  /shop/register:
- *    post:
- *      summary: create new shop
- *      consume:
- *        - application/json
- *      requestBody:
- *       content:
+ *   /shop/register:
+ *     post:
+ *       summary: create new shop
+ *       consume:
+ *         - application/json
+ *       requestBody:
+ *         content:
  *         application/json:
  *           schema:
  *             $ref: '#/definitions/Shop'
  *      responses:
  *        200:
  *          description: return new registered shop
+ *          content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/definitions/Shop'
  *        400:
  *          description: error occurs.
  */
 router.post('/shop/register', shopController.register)
-// [ ] route: /shop/login
+
+
+router.post('/shop/login', shopController.register)
 
 /**
  * @swagger
@@ -212,9 +370,18 @@ router.post('/shop/register', shopController.register)
  *           name: _id
  *           type: number
  *           required: true
+ *       requestBody:
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/definitions/Token'
  *       responses:
  *         200:
  *           description: return a shop
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/definitions/Shop'
  *         400:
  *           description: fetched failed
  */
@@ -228,7 +395,7 @@ router.get('/shop/getById', shopController.getById)
  *       summary: get all shops
  *       responses:
  *         200:
- *           description: return all shops
+ *           description: return array of shops
  *         400:
  *           description: fetched failed
  */
@@ -237,24 +404,28 @@ router.get('/shop/get', shopController.get)
 /**
  * @swagger
  * paths:
- *  /shop/update:
- *    put:
- *      summary: edit shop
- *      parameters:
- *        - in: query
- *          name: _id
- *          type: number
- *          required: true
- *      requestBody:
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/definitions/Shop'  
- *      responses:
- *        200:
- *          description: return an edited shop
- *        400:
- *          description: error occurs.
+ *   /shop/update:
+ *     put:
+ *       summary: edit shop
+ *       parameters:
+ *         - in: query
+ *           name: _id
+ *           type: number
+ *           required: true
+ *       requestBody:
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/definitions/ShopInput'
+ *       responses:
+ *         200:
+ *           description: return an edited shop
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/definitions/Shop'
+ *         400:
+ *           description: error occurs.
  */
 router.put('/shop/update', shopController.update)
 
@@ -272,6 +443,10 @@ router.put('/shop/update', shopController.update)
  *       responses:
  *         200:
  *           description: return a deleted shop
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/definitions/Shop'
  *         400:
  *           description: fetched failed
  */
@@ -280,20 +455,23 @@ router.delete('/shop/delete', shopController.deleteByID)
 /**
  * @swagger
  * paths:
- *  /customer/register:
- *    post:
- *      summary: create new customer
- *      requestBody:
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/definitions/Customer'
- *      responses:
- *        200:
- *          description: return new registered customer
- *        400:
- *          description: error occurs.
- *            
+ *   /customer/register:
+ *     post:
+ *       summary: create new customer
+ *       requestBody:
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/definitions/Customer'
+ *       responses:
+ *         200:
+ *           description: return new registered customer
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/definitions/Customer'
+ *         400:
+ *           description: error occurs.
  */
 router.post('/customer/register', customerController.register)
 
@@ -327,9 +505,18 @@ router.post('/customer/login', customerController.login)
  *           name: _id
  *           type: number
  *           required: true
+ *       requestBody:
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/definitions/Token'
  *       responses:
  *         200:
  *           description: return a customer
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/definitions/Customer'
  *         400:
  *           description: fetched failed
  */
@@ -343,7 +530,7 @@ router.get('/customer/getById', customerController.getById)
  *       summary: get all customer
  *       responses:
  *         200:
- *           description: return all customer
+ *           description: return array of customer
  *         400:
  *           description: fetched failed
  */
@@ -364,7 +551,7 @@ router.get('/customer/get', customerController.get)
  *         content:
  *           application/json:
  *             schema:
- *               $ref: '#/definitions/Customer'
+ *               $ref: '#/definitions/CustomerInput'
  *       responses:
  *         200:
  *           description: return edited customer
@@ -393,7 +580,6 @@ router.put('/customer/update', customerController.update)
 router.delete('/customer/delete', customerController.deleteByID)
 
 // [ ] route: /logout
-router.get('/customer/recommend', middlewareAuth, recommendController.calculateShop)
 
 /**
  * @swagger
@@ -406,13 +592,18 @@ router.get('/customer/recommend', middlewareAuth, recommendController.calculateS
  *           name: _id
  *           type: number
  *           required: true
+ *       requestBody:
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/definitions/Token'
  *       responses:
  *         200:
  *           description: return list of recommended shops
  *         400:
  *           description: error occurs
  */
-router.get('/customer/recommend', recommendController.calculateShop)
+router.get('/customer/recommend', middlewareAuth, recommendController.calculateShop)
 
 // for dev
 router.post('/shop/inserttest', shopController.randomInsertShop)
